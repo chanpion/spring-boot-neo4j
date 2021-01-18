727eefc85d64751c45038ff27193004453959c45
April.Chen <forchpeng@163.com>
2021-01-18 20:39:42 +0800

    Initial commit

---
+++ src/main/java/com/chanpion/kg/neo4j/Neo4jApplication.java
@@ -0,0 +1,16 @@
+package com.chanpion.kg.neo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author April Chen
+ * @date 2021/1/18 19:41
+ */
+@SpringBootApplication
+public class Neo4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4jApplication.class, args);
+    }
+}
---
+++ src/main/java/com/chanpion/kg/neo4j/controller/Neo4jController.java
@@ -0,0 +1,47 @@
+package com.chanpion.kg.neo4j.controller;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.collections4.IteratorUtils;
+import org.neo4j.driver.AccessMode;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.SessionConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author April Chen
+ * @date 2021/1/18 19:46
+ */
+@RestController
+@RequestMapping(value = "/neo4j", method = {RequestMethod.GET, RequestMethod.POST})
+public class Neo4jController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Neo4jController.class);
+
+    @Resource
+    private Driver driver;
+
+    @RequestMapping("/search")
+    public Object search() {
+        LOGGER.info("search");
+        SessionConfig sessionConfig = SessionConfig.builder().withDefaultAccessMode(AccessMode.READ).build();
+        try (Session session = driver.session(sessionConfig)) {
+            return session.run("MATCH (m:个股) RETURN m ORDER BY m.name ASC").stream()
+                    .map(r -> r.get("m").asNode())
+                    .map(n -> {
+                        Map<String,Object> map = n.asMap();
+                        map.put("labels", IterableUtils.toList(n.labels()));
+                    return  map;})
+
+                    .collect(Collectors.toList());
+        }
+    }
+}
